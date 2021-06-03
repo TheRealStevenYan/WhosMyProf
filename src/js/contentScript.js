02d2794337bba9c1e054c08f16a0ca4dfd778b76
TheRealStevenYan <33842907+TheRealStevenYan@users.noreply.github.com>
2021-06-02 23:18:52 -0700

    State as of 06/02/2021: Scrapping the node.js idea for now. Going to develop a separate application to save prof data in a JSON file, where the extension will read it.

--- src/js/contentScript.js
+++
@@ -1,6 +0,0 @@
-// Loads contentMain.js, which loads the rest of the module.
-(async () => {
-    const src = chrome.runtime.getURL('./contentMain.js');
-    const contentScript = await import(src);
-    contentScript.main();
-})();
--- src/js/pageCheck.js
+++
@@ -1,9 +0,0 @@
-export function isSectionsPage(url) {
-    let bool = new URLSearchParams(url).has("subj-section");
-    return bool;
-}
-
-export function isSectionsPage(url) {
-    let bool = new URLSearchParams(url).has("subj-course");
-    return bool;
-}
